bee74785894fab753f4d4eb643ede4a5bc670484
feeRnt <81442162+feeRnt@users.noreply.github.com>
2026-03-05 11:13:40 +0000

    Enable UVD and VCE en/decoder blocks in cik.c
    These two blocks were disabled in
    drivers/gpu/drm/amd/amdgpu/cik.c for
    Liverpool and Gladius GPUs.
    
    Turned on their IP blocks for testing.
    
    Might just break, but need to see
    if hw encoders and decoders can function.
    
    Signed-off-by: feeRnt <81442162+feeRnt@users.noreply.github.com>

--- drivers/gpu/drm/amd/amdgpu/cik.c
+++ drivers/gpu/drm/amd/amdgpu/cik.c
@@ -2685,8 +2685,8 @@ int cik_set_ip_blocks(struct amdgpu_device *adev)
 			amdgpu_device_ip_block_add(adev, &dce_v8_1_ip_block);
 		amdgpu_device_ip_block_add(adev, &gfx_v7_1_ip_block);
 		amdgpu_device_ip_block_add(adev, &cik_sdma_ip_block);
-		/*amdgpu_device_ip_block_add(adev, &uvd_v4_2_ip_block)*/;
-		/*amdgpu_device_ip_block_add(adev, &vce_v2_0_ip_block)*/;
+		amdgpu_device_ip_block_add(adev, &uvd_v4_2_ip_block); /* These two were	*/
+		amdgpu_device_ip_block_add(adev, &vce_v2_0_ip_block); /* disabled. Let's c */
 		break;
 	case CHIP_GLADIUS:
 		amdgpu_device_ip_block_add(adev, &cik_common_ip_block);
