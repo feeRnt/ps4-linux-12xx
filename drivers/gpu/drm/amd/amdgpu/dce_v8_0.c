15c54133f31351301ad6b31757a40c6eedb38b33
feeRnt <81442162+feeRnt@users.noreply.github.com>
2026-04-26 13:18:24 +0000

    amd/dce_v8_0: Use 6 CRTCs for LIVERPOOL

--- drivers/gpu/drm/amd/amdgpu/dce_v8_0.c
+++ drivers/gpu/drm/amd/amdgpu/dce_v8_0.c
@@ -420,10 +420,10 @@ static int dce_v8_0_get_num_crtc(struct amdgpu_device *adev)
 	switch (adev->asic_type) {
 	case CHIP_BONAIRE:
 	case CHIP_HAWAII:
+	case CHIP_LIVERPOOL:
 	case CHIP_GLADIUS:
 		num_crtc = 6;
 		break;
-	case CHIP_LIVERPOOL:
 	case CHIP_KAVERI:
 		num_crtc = 4;
 		break;
